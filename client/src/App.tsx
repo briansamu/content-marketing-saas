8854308b7d552ba95aefdb7e54ee2e8b6bf20454
Brian Samu <brian@briansamu.com>
2025-05-05 08:07:00 -0400

    basic login form designed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Login from './pages/Login/Login'
+import { ThemeProvider } from './components/theme-provider'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Login />
+      </ThemeProvider>
     </>
   )
 }
