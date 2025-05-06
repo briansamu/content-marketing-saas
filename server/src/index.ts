48c4f5e2f17db0ad5b83488ca0a99155fa4ca415
Brian Samu <brian@briansamu.com>
2025-05-06 14:08:50 -0400

    init backend

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import multer from "multer";
+
+dotenv.config();
+
+const app = express();
+const upload = multer();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(upload.none());
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
