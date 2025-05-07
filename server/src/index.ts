aa400af6bf480624dc5f215d647b69c54b09a14f
Brian Samu <brian@briansamu.com>
2025-05-07 16:38:14 -0400

    fix:postgres on mac

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ app.use(upload.none());
 
 syncDatabase();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
