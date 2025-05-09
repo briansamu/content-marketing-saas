4ed2fe0bfc6fd6543e75fa15665b5d42779f7782
Brian Samu <brian@briansamu.com>
2025-05-08 23:13:51 -0400

    Remove unused Dashboard.tsx component

--- client/src/pages/Dashboard/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router';
-
-// This component now just redirects to the new dashboard/overview route
-const Dashboard = () => {
-  return <Navigate to="/app/dashboard/overview" replace />;
-};
-
-export default Dashboard;
