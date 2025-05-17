2adf3d864907f520a5c511c1554093a9c823978f
Brian Samu <brian@briansamu.com>
2025-05-16 23:06:59 -0400

    Add SEO API service and root package dependencies

---
+++ server/src/services/seoApiService.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import logger from '../utils/logger';
+
+dotenv.config();
+
+const DATAFORSEO_API_LOGIN = process.env.DATAFORSEO_API_LOGIN;
+const DATAFORSEO_API_PASSWORD = process.env.DATAFORSEO_API_PASSWORD;
+
+class SeoApiService {
+
+}
